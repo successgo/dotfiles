f5e9bfb4c7c00c2332a9d789b51aa22a103802b9
Success Go <successgdc@gmail.com>
2021-09-20 10:19:03 +0800

    Cleanup PATH env setting.

--- .gdc.export.sh
+++ .gdc.export.sh
@@ -19,93 +19,74 @@ export KEYTIMEOUT=1
 # make man window not too long and not too short
 export MANWIDTH=100
 
-# reset path
-# for macOS: /usr/local/bin better be in front of /bin
+# Reset PATH
+# for macOS: /usr/local/bin would better be in front of /bin
 export PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/sbin:/usr/sbin
 
-# home bin
+# Add home bin
 if [[ -d $HOME/bin ]]; then
   export PATH=$PATH:$HOME/bin
 fi
 
-# dotnet sdk
-if [[ -d $HOME/dotnet ]]; then
-  export PATH=$PATH:$HOME/dotnet
-fi
-# dotnet tools
-if [[ -d $HOME/.dotnet/tools ]]; then
-  export PATH=$PATH:$HOME/.dotnet/tools
-fi
-
-# composer vendor bin
+# Add composer global vendor bin for linux
 if [[ -d $HOME/.config/composer/vendor/bin ]]; then
   export PATH=$PATH:$HOME/.config/composer/vendor/bin
 fi
-
-# composer vendor bin for macOS
+# Add composer global vendor bin for macOS
 if [[ -d $HOME/.composer/vendor/bin ]]; then
   export PATH=$PATH:$HOME/.composer/vendor/bin
 fi
 
-# symfony bin
+# Add symfony bin
 if [[ -d $HOME/.symfony/bin ]]; then
   export PATH=$PATH:$HOME/.symfony/bin
 fi
 
-# cargo bin
-if [[ -d $HOME/.cargo/bin ]]; then
-  export PATH=$PATH:$HOME/.cargo/bin
-fi
-
-# nodejs bin
-if [[ -d /opt/node ]]; then
-  export PATH=$PATH:/opt/node/bin
-fi
-
-# JDK bin
+# Add JDK and it's bin for local install
 if [[ -d /opt/jdk ]]; then
   export JAVA_HOME=/opt/jdk
   export PATH=$PATH:$JAVA_HOME/bin
 fi
 
-# DotNET SDK
+# Add dotnet sdk and it's bin for local install
 if [[ -d $HOME/dotnet ]]; then
   export DOTNET_ROOT=$HOME/dotnet
-  export PATH=$PATH:$DOTNET_ROOT
+  export PATH=$PATH:$HOME/dotnet
+fi
+# Add dotnet tools
+if [[ -d $HOME/.dotnet/tools ]]; then
+  export PATH=$PATH:$HOME/.dotnet/tools
 fi
 
-# go bin
-if [[ -d /opt/go ]]; then
+# Add go sdk and it's bin for local install
+if [[ -d /usr/local/go/bin ]]; then
+  export GO_HOME=/usr/local/go
+  export PATH=$PATH:$GO_HOME/bin
+fi
+# Add go sdk and it's bin for local install
+if [[ -d /opt/go/bin ]]; then
   export PATH=$PATH:/opt/go/bin
 fi
-
-# GOPATH
+# Set GOPATH
 if [[ -d $HOME/go ]]; then
-  export GOPATH=~/go
+  export GOPATH=$HOME/go
   export PATH=$PATH:$GOPATH/bin
 fi
 
-# make special love for macOS
+# Add cargo bin
+if [[ -d $HOME/.cargo/bin ]]; then
+  export PATH=$PATH:$HOME/.cargo/bin
+fi
+
+# Add nodejs bin for local install
+if [[ -d /opt/node ]]; then
+  export PATH=$PATH:/opt/node/bin
+fi
+
+# Overwrite default install for macOS
 if [[ `uname -s` = 'Darwin' ]]; then
-  # system has default curl which cannot be overwritten
   if [[ -d /usr/local/opt/curl ]]; then
-    CURL_HOME=/usr/local/opt/curl
-    export PATH=$CURL_HOME/bin:$PATH
-  fi
-  # system has default ruby which cannot be overwritten
-  if [[ -d /usr/local/opt/ruby ]]; then
-    RUBY_HOME=/usr/local/opt/ruby
-    export PATH=$RUBY_HOME/bin:$PATH
-  fi
-  # system has default php which cannot be overwritten
-  if [[ -d /usr/local/opt/php ]]; then
-    PHP_HOME=/usr/local/opt/php
-    export PATH=$PHP_HOME/bin:$PHP_HOME/sbin:$PATH
-  fi
-  # golang tools
-  if [[ -d /usr/local/go ]]; then
-    export GO_HOME=/usr/local/go
-    export PATH=$GO_HOME/bin:$PATH
+    export PATH=/usr/local/opt/curl/bin:$PATH
   fi
 fi
 
@@ -115,6 +96,6 @@ if [[ -d $HOME/.jetbrains/phpstorm ]]; then
   export PATH=$PATH:$PHPSTORM_HOME/bin
 fi
 
-# unset http proxy for sure on bootstrap and use them as need
+# Unset http proxy for sure on bootstrap and use them as need
 unset {http_proxy,https_proxy,no_proxy}
 unset {HTTP_PROXY,HTTPS_PROXY,NO_PROXY}
